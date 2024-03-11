7624787ff240c8ed0cc9c5a8d55ad68de4f07741
Service Web <sw@gssmail.com>
2024-03-11 14:41:24 -0500

    Commit changes in C:\Websites\Git\emarlow\4273\GCG_4273_WORKORDER.sql

---
+++ GCG_4273_WORKORDER.sql
@@ -0,0 +1,7 @@
+/* It was decided that this table was redundant and was scrapped from the completed project
+
+CREATE TABLE "GCG_4273_WORKORDER"(
+ "JOB" CHAR(6),
+ "SUFFIX" CHAR(3),
+ "FURNACE" CHAR(6));
+*/
